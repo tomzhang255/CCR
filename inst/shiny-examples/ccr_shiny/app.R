1708f80119a0513d7d63f250b00bb26ccabf4b2f
tomzhang255 <tomzhang255@gmail.com>
2023-02-13 00:51:39 -0500

    shiny clear env after shutdown

--- inst/shiny-examples/ccr_shiny/app.R
+++ inst/shiny-examples/ccr_shiny/app.R
@@ -42,23 +42,23 @@ ui <- dashboardPage(
 
 server <- function(input, output) {
   values <- reactiveValues()
-  
+
   output$d_col_ui <- renderUI({
     render_select_col_ui(input$d_file, "d_col", "User Data Column", values, "d_data")
   })
-  
+
   output$q_col_ui <- renderUI({
     render_select_col_ui(input$q_file, "q_col", "Questionnaire Column", values, "q_data")
   })
-  
+
   output$d_preview <- renderRHandsontable({
     render_hot(head(values$d_data, 10), input$d_col)
   })
-  
+
   output$q_preview <- renderRHandsontable({
     render_hot(head(values$q_data, 10), input$q_col)
   })
-  
+
   calc_reactive <- eventReactive(input$calc, {
     # first make sure all inputs are not null
     if (is.null(input$d_file) | is.null(input$q_file)) {
@@ -67,7 +67,7 @@ server <- function(input, output) {
     }
 
     showNotification("Calculating; please wait...", type = "message")
-    
+
     tryCatch({
       console_text <- capture.output(res <- ccr_wrapper(values$d_data, input$d_file$name, input$d_col,
                                                         values$q_data, input$q_file$name, input$q_col),
@@ -78,16 +78,16 @@ server <- function(input, output) {
     })
 
     output$console <- renderPrint(console_text)
-    
+
     values$res <- res
-    
+
     return(rhandsontable(head(res, input$res_rows), height = 150))
   })
-  
+
   output$result_hot <- renderRHandsontable({
     calc_reactive()
   })
-  
+
   # using an extra layer in order to check result availability before download
   observeEvent(input$download, {
     if (is.null(values$res)) {
@@ -96,7 +96,7 @@ server <- function(input, output) {
       runjs("$('#download_hidden')[0].click();")
     }
   })
-  
+
   output$download_hidden <- downloadHandler(
     filename = function() { "ccr_result.csv" },
     content = function(file) {
@@ -105,4 +105,10 @@ server <- function(input, output) {
   )
 }
 
-shinyApp(ui, server)
+shinyApp(ui, server, onStart = function() {
+  onStop(function() {  # clear shiny helper functions from env after shiny quits
+    rm(list = c("ccr_wrapper", "encode_column", "item_level_ccr",
+                "render_hot", "render_select_col_ui",
+                "validate_col_item_length", "validate_file"), envir = .GlobalEnv)
+  })
+})
